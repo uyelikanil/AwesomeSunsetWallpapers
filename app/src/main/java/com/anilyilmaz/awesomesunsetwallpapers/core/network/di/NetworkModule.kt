b5f5d600edbdf6736bca465ed7eaac35aa2cb828
Anıl Yılmaz <uyelikanil@gmail.com>
2023-05-13 04:00:37 +0300

    Add api key to api

--- app/src/main/java/com/anilyilmaz/awesomesunsetwallpapers/core/network/di/NetworkModule.kt
+++ app/src/main/java/com/anilyilmaz/awesomesunsetwallpapers/core/network/di/NetworkModule.kt
@@ -1,11 +1,16 @@
 package com.anilyilmaz.awesomesunsetwallpapers.core.network.di
 
+import android.webkit.HttpAuthHandler
+import com.anilyilmaz.awesomesunsetwallpapers.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Call
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.internal.addHeaderLenient
 import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 
@@ -15,10 +20,14 @@ object  NetworkModule {
     @Provides
     @Singleton
     fun okHttpCallFactory(): Call.Factory = OkHttpClient.Builder()
+        .addInterceptor(Interceptor {chain ->
+            val request = chain.request().newBuilder().addHeader("Authorization",
+                "Bearer ${BuildConfig.API_KEY}").build()
+        chain.proceed(request)})
         .addInterceptor(
             HttpLoggingInterceptor()
                 .apply {
-                    setLevel(HttpLoggingInterceptor.Level.BODY)
+                    setLevel(HttpLoggingInterceptor.Level.BASIC)
                 },
         )
         .build()
