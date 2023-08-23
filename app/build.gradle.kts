26b9ed440561b90d189901704e3800e870c6d64f
Anıl Yılmaz <uyelikanil@gmail.com>
2023-08-23 18:04:06 +0300

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.anilyilmaz.awesomesunsetwallpapers"
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
     }
 
     buildTypes {
