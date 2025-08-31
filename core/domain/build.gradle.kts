d74bc117492652c5467ee6f1d1393c69d26d5cf1
uyelikanil <uyelikanil@gmail.com>
2025-08-31 16:17:42 +0300

    Migrate domain module as a shared module

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
-    id("awesomesunsetwallpapers.android.library")
-    id("awesomesunsetwallpapers.android.koin")
+    id("awesomesunsetwallpapers.multiplatform.core.library")
+    id("awesomesunsetwallpapers.multiplatform.koin")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":core:model"))
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        commonTest.dependencies {
+            implementation(project(":core:testing"))
+            implementation(kotlin("test"))
+        }
+    }
 }
 
 android {
     namespace = "com.anilyilmaz.awesomesunsetwallpapers.core.domain"
-}
-
-dependencies {
-    implementation(project(":core:model"))
-    testImplementation(project(":core:testing"))
-
-    implementation(libs.kotlinx.coroutines.android)
-    testImplementation(kotlin("test"))
 }
