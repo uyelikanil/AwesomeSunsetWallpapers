0b7eed431f9cace5fc3ef80a578cc49f0be3176c
uyelikanil <uyelikanil@gmail.com>
2025-08-31 16:16:55 +0300

    Migrate testing module as a shared module

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -1,21 +1,31 @@
-plugins {id("awesomesunsetwallpapers.android.library")
+plugins {
+    id("awesomesunsetwallpapers.multiplatform.core.library")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":core:network"))
+            implementation(project(":core:model"))
+
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            api(libs.ktor.client.mock)
+            api(libs.androidx.test.runner)
+            api(libs.kotlinx.coroutines.test)
+            api(libs.junit4)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
 }
 
 android {
     namespace = "com.anilyilmaz.awesomesunsetwallpapers.core.testing"
-}
-
-dependencies {
-    implementation(project(":core:network"))
-    implementation(project(":core:model"))
-
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.ktor)
-    implementation(libs.ktor.okhttp)
-    implementation(libs.ktor.content.negotiation)
-    implementation(libs.ktor.kotlin.serialization)
-    api(libs.ktor.mock)
-    api(libs.androidx.test.runner)
-    api(libs.kotlinx.coroutines.test)
-    api(libs.junit4)
 }
