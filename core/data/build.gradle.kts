b1e9a32f192b5fa46e1cc32741e56a5b83f1d5ef
uyelikanil <uyelikanil@gmail.com>
2025-08-31 16:17:50 +0300

    Migrate data module as a shared module

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
-    id("awesomesunsetwallpapers.android.library")
-    id("awesomesunsetwallpapers.android.koin")
+    id("awesomesunsetwallpapers.multiplatform.core.library")
+    id("awesomesunsetwallpapers.multiplatform.koin")
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":core:network"))
+            implementation(project(":core:common"))
+            implementation(project(":core:model"))
+            implementation(project(":core:domain"))
+
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        commonTest.dependencies {
+            implementation(project(":core:testing"))
+        }
+    }
 }
 
 android {
     namespace = "com.anilyilmaz.awesomesunsetwallpapers.core.data"
-}
-
-dependencies {
-    implementation(project(":core:network"))
-    implementation(project(":core:common"))
-    implementation(project(":core:model"))
-    implementation(project(":core:domain"))
-    testImplementation(project(":core:testing"))
-
-    implementation(libs.kotlinx.coroutines.android)
 }
